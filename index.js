7c9eb2b90cf363575d965b7292477a242378519c
Rahman <rahmanromli@gmail.com>
2019-05-08 02:38:46 +0800

    Delete note

--- index.js
+++ index.js
@@ -20,6 +20,18 @@ server.addService(notesProto.NoteService.service, {
         note.id = uuidv1()
         notes.push(note)
         callback(null, note)
+    },
+    delete: (call, callback) => {
+        let existingNoteIndex = notes.findIndex((n) => n.id==call.request.id)
+        if (existingNoteIndex != -1) {
+            notes.splice(existingNoteIndex, 1)
+            callback(null, {})
+        } else {
+            callback({
+                code: grpc.status.NOT_FOUND,
+                details: "Not found"
+            })
+        }
     }
 })
 
